469255824fb77f12368fc208773f0b5b565165c9
mayarinhan <103398087+mayarinhan@users.noreply.github.com>
2022-08-23 12:19:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var i = 0;
 
+let textad = '';
+let tituload = '';
+
+
 function coluna() {
 
-  
+
   let coluna = document.createElement("div");
   coluna.setAttribute('id', 'coluna');
   coluna.setAttribute('class', 'coluna');
@@ -14,68 +18,101 @@ function coluna() {
   var escrita = document.createTextNode("+ card");
 
   botao.appendChild(escrita);
-  botao.style.height="6%";
-  botao.style.backgroundColor="#7d71ea";
-  botao.style.color="#c6fbc6";
-  botao.style.fontSize="100%";
-  botao.style.borderRadius="15%";
-  botao.style.borderColor="#7d71eb";
-  botao.style.margin="2%";
+  botao.style.height = "6%";
+  botao.style.backgroundColor = "#7d71ea";
+  botao.style.color = "#c6fbc6";
+  botao.style.fontSize = "100%";
+  botao.style.borderRadius = "15%";
+  botao.style.borderColor = "#7d71eb";
+  botao.style.margin = "2%";
   botao.setAttribute('onclick', 'createCard()');
   coluna.appendChild(botao);
 }
 
-function criar() {
-  let titulo_card = getElementById('tituloadd').value;
-  let text_card = getElementById('textadd').value;
-
-  let title = getElementById('tituloadd');
-  let textocard = getElementById('textadd');
-
-  var h1 = document.createElement("h1");
-  var h1_2 = document.createElement("h1");
-
-  var textoh1 = document.createTextNode(titulo_card);
-  var textoh2 = document.createTextNode(text_card);
-
-  h1.appendChild(titulo_card);
-  h1_2.appendChild(text_card);
-  title.appendChild(h1);
-  textocard.appendChild(h1_2);
-}
 
 function createCard() {
-  
+
   let card = document.createElement("div");
   card.setAttribute('id', 'info');
 
   let tituloadd = document.createElement("input");
-  tituloadd.setAttribute('id', 'tituloadd');
+  tituloadd.setAttribute('id', 'tituload');
+  tituloadd.setAttribute('placeholder', 'Titulo');
+
+  tituload = tituloadd.value;
 
   let textadd = document.createElement("input");
-  textadd.setAttribute('id', 'textadd');
+  textadd.setAttribute('id', 'textad');
+  textadd.setAttribute('placeholder', 'Texto');
 
-  let criar = document.createElement("button");
+  textad = textadd.value;
+
+  let criar = document.createElement("input");
   criar.setAttribute('id', 'criar');
   criar.setAttribute('onclick', 'criar()');
+  criar.setAttribute('type', 'button');
+  criar.setAttribute('value', 'Criar')
 
   card.appendChild(tituloadd);
   card.appendChild(textadd);
   card.appendChild(criar);
 
+  tituloadd.style.display = 'block';
+  textadd.style.display = 'block';
+
   let coluna = document.getElementById("coluna");
   coluna.appendChild(card);
 
-  if (tituloadd = null) {
-    alert('Você precisa colocar um titulo!!!!');
-  }
-  if (textadd = null) {
-    alert('Você precisa colocar texto!!!!');
-  }
-} 
+  card.style.marginTop = '10px';
+  card.style.marginLeft = '5px';
+  criar.style.marginTop = '10px';
+  textadd.style.marginTop = '10px';
 
+
+}
+
+function criar() {
+  let text_card = '';
+  let titulo_card = '';
+  let tituload = document.getElementById('tituload');
+  let textad = document.getElementById('textad');
+  let card = document.getElementById('cardadd')
+
+
+  if (tituload.value == "") {
+    alert('Você precisa colocar um titulo!!!!');
+  } else {
+    titulo_card = tituload.value;
+  }
+
+  if (textad.value == "") {
+    alert('Você precisa colocar texto!!!!');
+  } else {
+    text_card = textad.value;
+  }
+
+  var h1 = document.createElement("h1");
+  var h1_2 = document.createElement("h1");
+
+
+  var textoh1 = document.createTextNode(titulo_card);
+  var textoh2 = document.createTextNode(text_card);
+
+  h1.appendChild(textoh1);
+  h1_2.appendChild(textoh2);
+
+  document.getElementById('coluna').appendChild("div");
+
+  document.getElementById('cardadd').appendChild(h1);
+  document.getElementById('cardadd').appendChild(h1_2);
+
+
+  h1.style.display = 'block';
+
+
+}
 function cardadd() {
-  
+
   let cardadd = document.createElement("div");
   cardadd.setAttribute('id', 'cardadd');
 
@@ -88,14 +125,11 @@ function cardadd() {
 
   let div = document.getElementById("card_add");
   div.appendChild(cardadd);
+
 }
 
 function add() {
   coluna();
 }
 
-function chamar(){
-  cardadd();
-}
-
-chamar();
+cardadd();
